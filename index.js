3455d3b0e56d0fa4b838a4827b0941975c9aea54
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-11-15 14:40:42 +0900

    simple api server

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// ライブラリ読み込み
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//body-parserの設定
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000; // port番号を指定
+
+
+// GET http://localhost:3000/api/v1/
+app.get('/api/v1/',function(req,res){
+    res.json({
+        message:"Hello,world"
+    });
+});
+
+//サーバ起動
+app.listen(port);
+console.log('listen on port ' + port);
