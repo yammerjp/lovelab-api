57d87b2693157966784cd7a1c5a7ce608654a4fb
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-11-21 14:29:10 +0900

    Add GET /invitations?inviteeuserid=:id

--- src/routes/v1/invitations.ts
+++ src/routes/v1/invitations.ts
@@ -32,8 +32,23 @@ router.post("/", (req, res) => {
   );
 });
 
-// GET https://lovelab.2n2n.ninja/api/v1/users/:userid/invitations
+// GET https://lovelab.2n2n.ninja/api/v1/invitations?inviteeuserid=userid
 //  招待をすべて取得 自分への招待のみ取得可能
+router.get("/", (req, res) => {
+  const inviteeuserid = parseInt(req.query.inviteeuserid, 10);
+  if (Number.isNaN(inviteeuserid) || inviteeuserid < 0) {
+    res.json({ error: true, errorMessage: "invalid query of userid" });
+    return;
+  }
+  Invitations.findAll({ where: { inviteeuserid } }).then(invitations => {
+    if (invitations === null) {
+      res.json({ error: true, errorMessage: "DataBase return null" });
+      return;
+    }
+    res.json(invitations);
+  });
+});
+
 // PUT https://lovelab.2n2n.ninja/api/v1/users/:userid/invitations/:invitationid
 //  招待を承諾/拒否 自分への招待のみ編集可能
 
