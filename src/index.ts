868288c34d1853ef63fc8cabc291b322baac75b8
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-11-24 23:09:43 +0900

    Devide authorization source code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
-import * as sha256 from "sha256";
 import connectDataBase from "./db";
-// import authorization from "./middleware/authorization";
-import { Tokens } from "./models/tokens";
+import authorization from "./middleware/authorization";
 import routerV1 from "./routes/v1/index";
 import routerV1authed from "./routes/v1/authed/index";
 
@@ -15,32 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 3000;
+
 app.use("/api/v1", routerV1);
 
-app.use("/api/v1/authed", (req, res, next) => {
-  console.log();
-  const httpHeaderAuth = req.get("Authorization");
-  if (httpHeaderAuth === undefined || /^Beaer /.test(httpHeaderAuth)) {
-    res.json({ error: true, errorMessage: "Authorization type is Beaer" });
-    return;
-  }
-  const token = httpHeaderAuth.split(" ")[1];
-  Tokens.findByPk(token).then(tokenObj => {
-    if (tokenObj === null) {
-      res.json({ error: true, errorMessage: "token is invalid" });
-      return;
-      // TODO: 日付をチェック
-    }
-    req.body.userid = tokenObj.userid;
-    console.log(`\n\n tokenObj.userid: ${tokenObj.userid}\n\n`);
-    console.log(`\n\n req.body.userid: ${req.body.userid}\n\n`);
-    next();
-
-    if (false) {
-      console.log(res.json());
-    }
-  });
-});
+// Authorization
+app.use("/api/v1/authed", authorization);
 
 app.use("/api/v1/authed", routerV1authed);
 
@@ -50,5 +27,3 @@ app.listen(port);
 console.log(`listen on port ${port}`);
 
 connectDataBase();
-const str = "user";
-console.log(sha256(str));
--- src/middleware/authorization.ts
+++ src/middleware/authorization.ts
@@ -1,25 +1,31 @@
 import * as express from "express";
 import { Tokens } from "../models/tokens";
 
-interface AuthorizedRequest extends express.Request {
-  userid: number;
-}
-
-const authorization = (req: AuthorizedRequest, res: any, next: Function) => {
+const authorization = (
+  req: express.Request,
+  res: express.Response,
+  next: Function
+): void => {
   console.log();
   const httpHeaderAuth = req.get("Authorization");
   if (httpHeaderAuth === undefined || /^Beaer /.test(httpHeaderAuth)) {
-    res.json({ error: true, errorMessage: "Authorization type is Beaer" });
+    res
+      .status(401)
+      .json({ errorCode: 1, errorMessage: "Authorization type is Beaer" });
     return;
+    // return文はtsコンパイラのために挿入
+    // httpHeaderAuth が undefinedであるときにこの先が実行されないことを明示
   }
   const token = httpHeaderAuth.split(" ")[1];
   Tokens.findByPk(token).then(tokenObj => {
     if (tokenObj === null) {
-      res.json({ error: true, errorMessage: "token is invalid" });
-      return;
       // TODO: 日付をチェック
+      res.status(403).json({ errorCode: 2, errorMessage: "token is invalid" });
+      return; // tscコンパイラにこの先が実行されないことを明示
     }
-    req.userid = tokenObj.userid;
+    req.body.userid = tokenObj.userid;
+    console.log(`\n\n tokenObj.userid: ${tokenObj.userid}\n\n`);
+    console.log(`\n\n req.body.userid: ${req.body.userid}\n\n`);
     next();
 
     if (false) {
