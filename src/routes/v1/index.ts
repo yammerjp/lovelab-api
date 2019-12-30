6f93e4a96abce041cd8a85e773f5cbc406e29c04
Keisuke Nakayama <baskk4.dt@gmail.com>
2019-12-30 10:50:56 +0900

    Add a endpoint of confirm to run API server

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
 import loginRouter from "./login";
 import signupRouter from "./signup";
+import helloRouter from "./hello";
 
 const router = express.Router();
 
+router.use("/", helloRouter);
 router.use("/signup", signupRouter);
 router.use("/login", loginRouter);
 
