cdee73598a87923e199eb02dbe140b1a29bfdaff
Keisuke Nakayama <baskk4.dt@gmail.com>
2020-01-23 02:04:50 +0900

    Fix to workflow

--- run.sh
+++ run.sh
@@ -8,28 +8,28 @@ PRODUCTION="production"
 if [ $1 = $LOCAL ];then
   echo "run $1"
   docker-compose build
-  docker-compose -f docker-compose.yml \
-                 -f docker-compose.override.yml \
-                 up
+  exec  docker-compose -f docker-compose.yml \
+                       -f docker-compose.override.yml \
+                       up
 #  docker-compose up
 elif [ $1 = $LOCALHTTPS ]; then
   echo "run $1"
   docker-compose build
-  docker-compose -f docker-compose.yml \
-                 -f docker-compose.local-https.yml \
-                 up
+  exec docker-compose -f docker-compose.yml \
+                      -f docker-compose.local-https.yml \
+                      up
 elif [ $1 = $TEST ]; then
   echo "run $1"
   docker-compose build
-  docker-compose -f docker-compose.yml \
-                 -f docker-compose.jest.yml \
-                 run --rm lovelab-api
+  exec docker-compose -f docker-compose.yml \
+                      -f docker-compose.jest.yml \
+                      run --rm lovelab-api
 elif [ $1 = $PRODUCTION ]; then
   echo "run $1"
   docker-compose build
-  docker-compose -f docker-compose.yml \
-                 -f docker-compose.production.yml \
-                 up
+  exec docker-compose -f docker-compose.yml \
+                      -f docker-compose.production.yml \
+                      up
 else
   echo 'example:'
   echo "$ $0 $LOCAL"
